7832091e56f2dd16dfe2320fc9b3d4a6de041b47
aircampro <147149143+aircampro@users.noreply.github.com>
2023-11-15 13:04:24 +0000

    Update vehicle_functions.py

--- plc_robot_coms/vehicle_functions.py
+++ plc_robot_coms/vehicle_functions.py
@@ -11,21 +11,21 @@ import math
 #
 def emulate_light_sensor( x_offset, y_offset, goal_point_x, goal_point_y ):
     # Determines whether the goal_point is in the field of view, and outputs 0 if it is not
-	#
-	if math.abs(math.atan2((goal_point_y - y_offset),(goal_point_x - x_offset))) >= (v_light_sensor_view_angle * 0.5):
-	    return 0.0
+    #
+    if math.abs(math.atan2((goal_point_y - y_offset),(goal_point_x - x_offset))) >= (v_light_sensor_view_angle * 0.5):
+	return 0.0
     
     # If you have a valid goal attitude
     # Calculate the square root of (goal_point.x - x_offset)^2 (goal_point.y - y_offset)^2 to find the distance between two points
-	#
-	distance = math.hypot((goal_point_x - x_offset), (goal_point_y - y_offset))
+    #
+    distance = math.hypot((goal_point_x - x_offset), (goal_point_y - y_offset))
 
     # We want to maximize the output value of the sensor while avoiding zeroing, so we return 1
     #	
-	if distance <= math.epsilon:
-	    return 1.0
+    if distance <= math.epsilon:
+        return 1.0
     else:
-	    ls = (1.0 / (distance * distance))
+	ls = (1.0 / (distance * distance))
         if (ls >= 1.0):
             return 1.0
         elif (ls <= 0.0):
