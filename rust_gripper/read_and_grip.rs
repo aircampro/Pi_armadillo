b10c61305800988b33e22b42f00d65b1586923f6
aircampro <147149143+aircampro@users.noreply.github.com>
2023-12-04 11:26:40 +0000

    Update read_and_grip.rs

--- rust_gripper/read_and_grip.rs
+++ rust_gripper/read_and_grip.rs
@@ -38,47 +38,6 @@ use std::time::Instant;
 
 use regex::Regex;
 
-// JSON sends position to get object name from server { "lat" : "23.4", "lon" : "0.032", "userId" : 1 } 
-// receives back json { "obj_name" : "unknown", "width" : 0 } or { "obj_name" : "small_box_3", "width" : 23.2 }
-//
-async fn post_json_pos_request(url: &str, v1: &str, v2: &str) -> std::io::Result<serde_json::Value> {
-    let echo_json: serde_json::Value = reqwest::Client::new()
-        .post(url)
-        .json(&serde_json::json!({
-            "lat": v1,
-            "lon": v2,
-            "userId": 1
-        }))
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    println!("{:#?}", echo_json);
-    Ok(echo_json)
-}
-// JSON sends name to get width from server { "obj_name" : "large_log_6", "userId" : 1 }
-// replies with a single value equivalent to the objects width or -1
-//
-async fn post_json_obj_request(url: &str, v1: &str) -> std::io::Result<f64; N> {
-    let ret_width: String = reqwest::Client::new()
-        .post(url)
-        .json(&serde_json::json!({
-            "obj_name": v1,
-            "userId": 1
-        }))
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    //let stri: String = String::from(ret_width); use if its not a text
-    //let num: f64 = stri.parse().unwrap();
-	let num: f64 = ret_width.parse().unwrap();
-    println!("width is {}", num);
-    Ok(num)
-}
-	
 // The [cfg(not(target_arch = "wasm32"))] above prevent building the tokio::main function
 // for wasm32 target, because tokio isn't compatible with wasm32.
 // If you aren't building for wasm32, you don't need that line.
@@ -165,31 +124,31 @@ fn main() -> Result<(), Box<std::error::Error>>) {
     // now grip the object
     //	
 	if ( result >= 0 ) {                                   // else it didnt find that object
-	        let width_from_server: [f64; N] = result;
+	    let width_from_server: [f64; N] = result;
 		let re = Regex::new(r"(?i)Object lost").unwrap();  // if we failed to grip try up to 3 times
 		let rep_cnt = 0;
 		
-	       // call the rust gripper controller program with the given width try 3 times as its in rust it might be just as easy to add it here
-	       // this is for flexible example purpose that we can change the gripper to one without a rust driver and use a shell to run it.
-	       //
-                let output = Command::new("/bin/cd")
-                .args(&["/home/robots/franka/rust/src"])
-                .spawn()
-                .expect("failed to change directory");	
-                println!("status: {}", output.status);
-                println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-                println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+	    // call the rust gripper controller program with the given width try 3 times as its in rust it might be just as easy to add it here
+	    // this is for flexible example purpose that we can change the gripper to one without a rust driver and use a shell to run it.
+	    //
+        let output = Command::new("/bin/cd")
+        .args(&["/home/robots/franka/rust/src"])
+        .spawn()
+        .expect("failed to change directory");	
+        println!("status: {}", output.status);
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 		while (rep_cnt < 3) {
-                   output = Command::new("/home/mark/.cargo/bin/cargo")
-                   .args(&["run", "--gripper_hostname", "my_gripper1", "--homing", 1, "--object_width", width_from_server])
-                   .spawn()
-                   .expect("failed to start gripper program`");	
-                   println!("status: {}", output.status);
-                   println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-                   println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-	           wait = speaker.speak(&output.stdout)?;
-                   wait.await
-                   if re.is_match(&output.stdout) {
+            output = Command::new("/home/mark/.cargo/bin/cargo")
+            .args(&["run", "--gripper_hostname", "my_gripper1", "--homing", 1, "--object_width", width_from_server])
+            .spawn()
+            .expect("failed to start gripper program`");	
+            println!("status: {}", output.status);
+            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+		    wait = speaker.speak(&output.stdout)?;
+            wait.await
+            if re.is_match(&output.stdout) {
 		        rep_cnt = rep_cnt + 1;
 		    } else {
 		        rep_cnt = 3;
@@ -199,4 +158,46 @@ fn main() -> Result<(), Box<std::error::Error>>) {
 	    println!("{} {}",".....object label read not found in database is it a ".red().bold(), text_label);
 	}
 
+}
+
+// JSON sends position to get object name from server { "lat" : "23.4", "lon" : "0.032", "userId" : 1 } 
+// receives back json { "obj_name" : "unknown", "width" : 0 } or { "obj_name" : "small_box_3", "width" : 23.2 }
+//
+async fn post_json_pos_request(url: &str, v1: &str, v2: &str) -> std::io::Result<serde_json::Value> {
+    let echo_json: serde_json::Value = reqwest::Client::new()
+        .post(url)
+        .json(&serde_json::json!({
+            "lat": v1,
+            "lon": v2,
+            "userId": 1
+        }))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", echo_json);
+    Ok(echo_json)
+}
+
+// JSON sends name to get width from server { "obj_name" : "large_log_6", "userId" : 1 }
+// replies with a single value equivalent to the objects width or -1
+//
+async fn post_json_obj_request(url: &str, v1: &str) -> std::io::Result<f64; N> {
+    let ret_width: String = reqwest::Client::new()
+        .post(url)
+        .json(&serde_json::json!({
+            "obj_name": v1,
+            "userId": 1
+        }))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    //let stri: String = String::from(ret_width); use if its not a text
+    //let num: f64 = stri.parse().unwrap();
+	let num: f64 = ret_width.parse().unwrap();
+    println!("width is {}", num);
+    Ok(num)
 }
