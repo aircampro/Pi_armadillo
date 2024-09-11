6c226d3ebb2f3ee7fcdf0c1d6b745b6852a461ed
aircampro <147149143+aircampro@users.noreply.github.com>
2024-09-11 17:16:01 +0100

    Update weather_past_present.py

--- data_manip/weather_past_present.py
+++ data_manip/weather_past_present.py
@@ -9,12 +9,81 @@
 # queries data from here https://amd.rd.naro.go.jp/opendap/AMD/
 #
 #
-#
 import AMD_Tools3 as AMD
 import DayLength as DL                         
 import datetime
 import sys
 
+import zipfile
+from zipfile import ZipFile
+
+# makes a zip file to upload to the ftp server
+#
+def make_zipfile(files_list, file_out='amd_data.zip'):
+    with ZipFile(file_out, 'w') as zf:
+        for f in files_list:
+            zf.write(f)                                  
+
+def unload_zipfile(in_file='act_temp_data.zip'):
+    with zipfile.ZipFile(in_file,'r') as inputFile:
+        inputFile.extractall()
+
+# ftp client 
+#
+from ftplib import FTP
+from datetime import datetime
+import shutil
+
+def ftp_client_actions(ftp_host = '10.0.1.5', ftp_user = 'root', ftp_pwd = 'passwdド', commands_list):
+
+    with FTP(ftp_host, ftp_user, ftp_pwd) as ftp:
+        for commnd in commands_list:
+            if commnd == "change_udir":
+	            ftp.cwd('/upload/')
+            if commnd == "change_ddir":
+	            ftp.cwd('/download/')
+            elif commnd == "get_files":
+	            file_list = []
+	            files_info = ftp.mlsd()
+	            for filename, info in files_info:
+                    if info['type'] == 'file':
+                        modify_dt = datetime.strptime(info['modify'],"%Y%m%d%H%M%S")
+                        file_list.append([filename,modify_dt])
+	            latest_file = max(file_list, key = lambda x:x[1])
+	            with open(latest_file[0], 'wb') as fp:
+                    ftp.retrbinary(f'RETR {latest_file[0]}', fp.write)
+            elif commnd == "put_files":
+                f_upl_name = str(datetime.now()).replace(' ','_').replace('-','_').replace('.','_').replace(':','_') + ".zip"
+                with open(f_upl_name, "wb") as f:
+                    ftp.retrbinary("RETR /amd_data.zip", f.write)
+                    
+# function to extract geoJson from the Ministry of Land, Infrastructure, Transport and Tourism's administrative
+#
+# Data is pulled from the Ministry of Land, Infrastructure, Transport and Tourism's administrative area information
+# Goal is to extract .geojson of the contents
+import urllib
+import json
+
+def get_mlit_info(url = 'https://nlftp.mlit.go.jp/ksj/gml/data/N03/N03-2019/N03-190101_47_GML.zip'):
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        data = res.read()
+	
+    # The data is in a zip file, so extract the contents and store them in a variable
+    with zipfile.ZipFile(io.BytesIO(data), 'r') as zip_data:
+        geojson_filepath = [name for name in zip_data.namelist() if '.geojson' in name][0]
+        with zip_data.open(geojson_filepath, 'r') as gj:
+            geojson_data = gj.read()
+        geojson = json.loads(geojson_data)
+        return geojson    
+
+def create_json_zip(d = {'name': 'deep insider', 'addr': 'japan'}):
+    content = json.dumps(d)
+    print(content)  # {"name": "deep insider", "addr": "japan"}
+
+    with ZipFile('mlit_data.zip', 'w') as zf:
+        zf.writestr('data.bin', content) 
+        
 # Para=[ Gv0, Th , A , Lc , B ,DVI*]
 # Function name: ʼHorie et al. (1995)ʼ
 # Varid phase: emergence to heading
@@ -66,16 +135,26 @@ model = model_list[model_chose]
 scenario = scenario_list[scen_chose]
 # choose date range for data
 TODAY=datetime.datetime.today()
-if sys.argc >= 4 :
-    SDATE=TODAY - datetime.timedelta(days=int(sys.argv[3]))
-    EDATE=TODAY + datetime.timedelta(days=int(sys.argv[4]))
+if len(sys.argv) >= 4 :
+    try:
+        days_before = int(sys.argv[3])
+        days_after = int(sys.argv[4])
+    except ValueError:
+        print("invalid parameters passed should be start_days end_days")
+        sys.exit(-1)        
+    SDATE=TODAY - datetime.timedelta(days=days_before)
+    EDATE=TODAY + datetime.timedelta(days=days_after)
 else:
     SDATE=TODAY - datetime.timedelta(days=20)
     EDATE=TODAY + datetime.timedelta(days=15)
 timedomain = [SDATE.strftime('%Y-%m-%d'), EDATE.strftime('%Y-%m-%d')]
 # this is the latitude longditude domain for the data
-if sys.argc >= 2 :
-    lalodomain = [float(sys.argv[1]), float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[2])]
+if len(sys.argv) >= 2 :
+    try:
+        lalodomain = [float(sys.argv[1]), float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[2])]
+    except ValueError:
+        print("invalid parameters passed should be °N °E")
+        sys.exit(-1)
 else:
     lalodomain = [35.0, 35.0, 135.0, 135.0]
 # select the meterological element to get example temperature, humidity, wind speed etc
@@ -131,24 +210,81 @@ for i in range(nlat):                                                     # Mesh
                 DVI[t,i,j] = DVI[t-1,i,j] + DVR
                 if DVI[t,i,j] >= 1.0:                                     # if outgoing date...
                     DOH[i,j] = frst + np.timedelta64(t,'D')               # Record the date.
-                else: #出穂から成熟までは…
+                else:                                                     # From emergence to maturity...
                     DVR = DVR12(Tmea[t-1,i,j])
                     DVI[t,i,j] = DVI[t-1,i,j] + DVR
-                    if DVI[t-1,i,j] < 2.0 and DVI[t,i,j] >= 2.0: #収穫適日ならば…
-                        DOR[i,j] = frst + np.timedelta64(t,'D') #日付を記録する．
-DVI[np.ma.where(DVI > 2.0)] = 2.0 #全体を見直してみて，DVI が2より大きいメッシュがあれ
+                    if DVI[t-1,i,j] < 2.0 and DVI[t,i,j] >= 2.0:          # If it is a good harvest day...…
+                        DOR[i,j] = frst + np.timedelta64(t,'D')           # Record the date..
+DVI[np.ma.where(DVI > 2.0)] = 2.0                                         # Review the entire area and if any mesh has DVI greater than 2
 
 # write out an excel sheet with the tmax data appended to it
-df = pd.read_excel("temperature.xlsx", index_col=0)
+df = pd.read_excel("temperature.xlsx", index_col=0)                       # read previous
 linking_df = pd.DataFrame({'Tmax':Tmax, 'time':tim0, 'lat':lat0, 'lon':lon0})
 excel_df = pd.concat([df, linking_df], axis=1)
 pd.io.formats.excel.ExcelFormatter.header_style = None
-excel_df.to_excel('temperature.xlsx', sheet_name='test')
+excel_df.to_excel('temperature.xlsx', sheet_name='temperture')            # append new data to previous file
 
 # write out an excel sheet with the DVI data appended to it
-df = pd.read_excel("dvi.xlsx", index_col=0)
-linking_df = pd.DataFrame({'DVI':DVI, 'time':tim, 'lat':lat0, 'lon':lon0})
+df = pd.read_excel("dvi.xlsx", index_col=0)                               # read in the previous starting point
+linking_df = pd.DataFrame({'DVI':DVI, 'time':tim, 'lat':lat, 'lon':lon})
 excel_df = pd.concat([df, linking_df], axis=1)
 pd.io.formats.excel.ExcelFormatter.header_style = None
 name = str(datetime.datetime.now()).replace(' ','_').replace(":","_").replace(".","_").replace("-","_")
-excel_df.to_excel(f'{name}(DVI).xlsx', sheet_name='dvi')
+excel_df.to_excel(f'{name}(DVI).xlsx', sheet_name='dvi')                  # write out the new file data with appended data to a new file named after datetime
+shutil.copyfile("dvi.xlsx", "dvi_backup.xlsx")
+shutil.copyfile(f'{name}(DVI).xlsx', "dvi.xlsx")
+
+# plot the data and save those plots as png files for upload to the ftp server 
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter,DayLocator
+import matplotlib.pylab as plt
+import matplotlib.colors as clr
+aspect = (lalodomain[3] - lalodomain[2]) / (lalodomain[1] - lalodomain[0]) + 0.5
+
+fig = plt.figure(num=None, figsize=(5*aspect, 5))                          # Define figure object (container)
+sclint = 3                                                                 # tick of color bar
+sclmin = np.datetime64(SDATE)                                              # minimum value of color bar
+sclmax = np.datetime64(EDATE)                                              # maximum value of color bar
+levels = np.arange(sclmin, sclmax+np.timedelta64(sclint,'D'), sclint)
+plt.axes(axisbg='0.8')                                                     # make background gray
+cmap = plt.cm.Spectral                                                     # Colormap with nickname. Invert with “_r” at the end.
+cmap.set_over('w', 1.0)                                                    # Color when the upper limit is exceeded
+cmap.set_under('k', 1.0)                                                   # Color when the upper limit is exceeded
+CF = plt.contourf(lon, lat, DOH, levels, cmap=cmap, extend='both')         # draw a distribution map
+CB = plt.colorbar(CF, format=DateFormatter('%b %d'))                       # draw a color bar
+plt.title('Date of Heading')                                               # title
+plt.savefig('Date_of_Heading.png', dpi=600)                                # save the figure as a bitmap image
+plt.show(fig)                                                              # display the figure
+plt.clf()
+
+fig = plt.figure(num=None, figsize=(5*aspect, 5))
+sclint = 3
+sclmin = np.datetime64(SDATE)
+sclmax = np.datetime64(EDATE)
+plt.axes(axisbg='0.8')
+levels = np.arange(sclmin, sclmax+np.timedelta64(sclint,'D'), sclint)
+cmap = plt.cm.Spectral
+cmap.set_over('w', 1.0)
+cmap.set_under('k', 1.0)
+CF = plt.contourf(lon, lat, DOR, levels, cmap=cmap, extend='both')
+CB = plt.colorbar(CF, format=DateFormatter('%b %d'))
+plt.title('Date of Ripe')
+plt.savefig('Date_of_Ripen.png', dpi=600)
+plt.show(fig)
+plt.clf()
+
+# Save the calculation results to a file
+AMD.PutNC_3D( DVI, tim, lat, lon, description='Rice Development Index', symbol='DVI', unit='-', filename='DVI.nc')
+
+# get mlit info and make zip file from it to upload to the ftp server with the rest
+gjd = get_mlit_info()
+create_json_zip(gjd)
+
+# zip this data and upload it, then download the actual data from the frp server and unzip it
+zip_list = [ f'{name}(DVI).xlsx', 'temperature.xlsx', 'mlit_data.zip', 'Date_of_Heading.png', 'Date_of_Ripen.png', 'DVI.nc' ]
+make_zipfile( zip_list )
+ftp_action_lst = [ "change_udir", "put_files", "change_ddir", "get_files" ]
+ftp_client_actions( ftp_action_lst )
+
+
+
