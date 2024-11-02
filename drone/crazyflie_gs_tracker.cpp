8591cccda20af593bc5bf7b5e44a563ee73f111d
aircampro <147149143+aircampro@users.noreply.github.com>
2024-11-02 13:33:41 +0000

    Update crazyflie_gs_tracker.cpp

--- drone/crazyflie_gs_tracker.cpp
+++ drone/crazyflie_gs_tracker.cpp
@@ -1,6 +1,15 @@
 // open CV example reading video feed and performing tracker
 // controlling a crazyflie drone
 //
+/*                                                                    to install openCV
+sudo apt-get update
+sudo apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools 
+sudo apt-get install libgstreamer-plugins-base1.0-0 
+sudo apt-get install gstreamer-video-1.0
+sudo apt-get install libgstreamer-plugins-base1.0-dev
+
+g++ `pkg-config --cflags opencv` <prog_name>.cpp -std=c++11 -g `pkg-config --libs opencv`
+*/
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
