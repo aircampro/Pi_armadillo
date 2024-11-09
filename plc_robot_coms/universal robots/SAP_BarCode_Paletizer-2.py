7910b2c3d084636caaa17411589da4184a948f54
aircampro <147149143+aircampro@users.noreply.github.com>
2024-11-09 12:05:50 +0000

    Add files via upload

--- plc_robot_coms/universal robots/SAP_BarCode_Paletizer-2.py
+++ plc_robot_coms/universal robots/SAP_BarCode_Paletizer-2.py
@@ -129,7 +129,21 @@ def place_box( coord, rotation, soc ):
         move_in_linear_joint_space(soc, (0,0,0,deg_2_rad(90),0,0), v=0.25, r=0.05, t=0)
     move_in_linear_joint_space(soc, coord, v=0.25, r=0.05, t=0)
     return True
-    
+
+def readCSVLines(line_boxs, line_level_data, line_rotations, line_level_rots):
+   # pallet_data[level=0][0][row=0] = "A,A,A,A,E" is bottom level first row
+   line =  line_boxs                                 # bottom level first row the csv data is either from a recipe file or SAP ?
+   d = line.split(",")
+   for zz in d:
+       line_level_data.append(ord(zz))
+   # pallet_data[level=0][1][row=0] = "0,0,0,0"      # rotations for that bottom level first row
+   line =  line_rotations                            # bottom level first row
+   d = line.split(",")
+   for zz in d:
+       line_level_rots.append(zz)
+
+# ============================= main =======================================       
+
 # choose the data related to the order from the SAP system
 order_str = '384XSD2321'
 sel = "SELECT QTY, FROM SAPA4H.USR02 WHERE ORDER = "+ order_str
@@ -180,160 +194,116 @@ for i, q in enumerate(qty):
         recipe_id = recipe_id | q<<64
         
 # now choose the pallet config (levels) boxs with (rotation) 0=none 1=90 degree
-level=0
-row=0
+level=0                                                 # start at bottom level (stack)
+row=0                                                   # start at first line (row)
 
 # make 3d arrays of the pallet set-up for each combination of boxes defined 
 # this is the optimal set for a specified selection of boxes
 # the data is followed by possible rotations for each box  
 # this could come from csv files produced for each recipe, hardcoded like here, or from SAP for that recipe
-#                
+# its hard coded to easy show you the format
+#              
 if recipe_id == (8 | 10<<8 | 10<<16):                  # 8 A, 10 B, 10 C
 
    # pallet_data[level=0][0][row=0] = "A,A,A,A,E" is bottom level first row
-   line =  "A,A,A,A,E"                                 # bottom level first row the csv data is either from a recipe file or SAP ?
-   d = line.split(",")
-   line1_level1_data = []
-   for zz in d:
-       line1_level1_data.append(ord(zz))
-   # pallet_data[level=0][1][row=0] = "0,0,0,0"           # rotations for that bottom level first row
-   line =  "0,0,0,0"                                  # bottom level first row
-   d = line.split(",")
+   # pallet_data[level=0][1][row=0] = "0,0,0,0"                 # rotations for that bottom level first row
+   line_boxs =  "A,A,A,A,E"                                     # bottom level first row the csv data is either from a recipe file or SAP ?
+   line_rotations =  "0,0,0,0"                                  # bottom level first row
    line1_level1_rots = []
-   for zz in d:
-       line1_level1_rots.append(zz)
-       
-   #pallet_data[level=0][0][row=1] = "B,B,C,C,C,E" 
-   line =  "B,B,C,C,C,E"                                 
-   d = line.split(",")
-   line2_level1_data = []
-   for zz in d:
-       line2_level1_data.append(ord(zz))       
-   # pallet_data[level][1][row] = "0,0,0,1,1"   
-   line =  "0,0,0,1,1"                                 
-   d = line.split(",")
+   line1_level1_data = []
+   readCSVLines(line_boxs, line1_level1_data, line_rotations, line1_level1_rots)
+   # pallet_data[level=0][1][row=0] = "0,0,0,0"                   # rotations for that bottom level first row
+ 
+   #pallet_data[level=0][0][row=1] = "B,B,C,C,C,E"  
+   line_boxs =  "B,B,C,C,C,E"                                  
+   # pallet_data[level=0][1][row=1] = "0,0,0,1,1"   
+   line_rotations =  "0,0,0,1,1"                              
    line2_level1_rots = []
-   for zz in d:
-       line2_level1_rots.append(zz)
-       
+   line2_level1_data = []
+   readCSVLines(line_boxs, line2_level1_data, line_rotations, line2_level1_rots)
+   
    #pallet_data[level=0][0][row=2] = "B,B,B,C,C,L" 
-   line =  "B,B,B,C,C,L"                                   
-   d = line.split(",")
-   line3_level1_data = []
-   for zz in d:
-       line3_level1_data.append(ord(zz))       
+   line_boxs =  "B,B,B,C,C,L"                                 
    #pallet_data[level=0][1][row=2] = "1,1,1,1,1"   
-   line =  "1,1,1,1,1"                                 
-   d = line.split(",")
+   line_rotations =  "1,1,1,1,1"                                 
    line3_level1_rots = []
-   for zz in d:
-       line3_level1_rots.append(zz)
-
-   #   pallet_data[level=1][0][row=0] = "A,A,A,A,E"
-   line = "A,A,A,A,E"                                 # second level first row
-   d = line.split(",")
-   line1_level2_data = []
-   for zz in d:
-       line1_level2_data.append(ord(zz))
-   #    pallet_data[level=1][1][row=0] = "0,0,0,0"           # rotations for that second level first row
-   line =  "0,0,0,0"                                  # bottom level first row
-   d = line.split(",")
-   line1_level2_rots = []
-   for zz in d:
-       line1_level2_rots.append(zz)
-       
-   #pallet_data[level=1][0][row=1] = "B,B,C,C,C,E"  
-   line =  "B,B,C,C,C,E"                                 
-   d = line.split(",")
-   line2_level2_data = []
-   for zz in d:
-       line2_level2_data.append(ord(zz))       
-   # pallet_data[level=1][1][row=1] = "0,0,0,1,1"   
-   line =  "0,0,0,1,1"                                 
-   d = line.split(",")
-   line2_level2_rots = []
-   for zz in d:
-       line2_level2_rots.append(zz)
-       
-   #pallet_data[level=1][0][row=2] = "B,B,B,C,C,F" 
-   line =  "B,B,B,C,C,F"                                   
-   d = line.split(",")
-   line3_level2_data = []
-   for zz in d:
-       line3_level2_data.append(ord(zz))       
-   #pallet_data[level=1][1][row=2] = "1,1,1,1,1"   
-   line =  "1,1,1,1,1"                                 
-   d = line.split(",")
-   line3_level2_rots = []
-   for zz in d:
-       line3_level2_rots.append(zz)       
-
-if recipe_id == (7 | 10<<8 | 10<<16):                    # 7 A, 10 B, 10 C
-   line =  "A,A,A,A,E"                                   # bottom level first row
-   d = line.split(",")
-   line1_level1_data = []
-   for zz in d:
-       line1_level1_data.append(ord(zz))                # store each box in decimal
-   line =  "0,0,0,0"                                    # rotations for that bottom level first row
-   d = line.split(",")
-   line1_level1_rots = []
-   for zz in d:
-       line1_level1_rots.append(zz)
-       
-   line =  "B,B,C,C,C,E"                                 
-   d = line.split(",")
-   line2_level1_data = []
-   for zz in d:
-       line2_level1_data.append(ord(zz))         
-   line =  "0,0,0,1,1"                                 
-   d = line.split(",")
-   line2_level1_rots = []
-   for zz in d:
-       line2_level1_rots.append(zz)
-        
-   line =  "B,B,B,C,C,L"                                   
-   d = line.split(",")
    line3_level1_data = []
-   for zz in d:
-       line3_level1_data.append(ord(zz))        
-   line =  "1,1,1,1,1"                                 
-   d = line.split(",")
-   line3_level1_rots = []
-   for zz in d:
-       line3_level1_rots.append(zz)
-
-   line = "A,A,Z,A,E"                                   # second level first row
-   d = line.split(",")
-   line1_level2_data = []
-   for zz in d:
-       line1_level2_data.append(ord(zz))
-   line =  "0,0,0,0"                                     # bottom level first row
-   d = line.split(",")
+   readCSVLines(line_boxs, line3_level1_data, line_rotations, line3_level1_rots)
+   
+   #   pallet_data[level=1][0][row=0] = "A,A,A,A,E" 
+   line_boxs =  "A,A,A,A,E"                                 
+   #    pallet_data[level=1][1][row=0] = "0,0,0,0"   
+   line_rotations =   "0,0,0,0"                                  
    line1_level2_rots = []
-   for zz in d:
-       line1_level2_rots.append(zz)
-       
-   line =  "B,B,C,C,C,E"                                 
-   d = line.split(",")
-   line2_level2_data = []
-   for zz in d:
-       line2_level2_data.append(ord(zz))       
-   line =  "0,0,0,1,1"                                 
-   d = line.split(",")
+   line1_level2_data = []
+   readCSVLines(line_boxs, line1_level2_data, line_rotations, line1_level2_rots)
+   
+   # pallet_data[level=1][0][row=1] = "B,B,C,C,C,E"
+   line_boxs =  "B,B,C,C,C,E"                                 
+   # pallet_data[level=1][1][row=1] = "0,0,0,1,1"    
+   line_rotations =   "0,0,0,1,1"                                   
    line2_level2_rots = []
-   for zz in d:
-       line2_level2_rots.append(zz)
-       
-   line =  "B,B,B,C,C,F"                                   
-   d = line.split(",")
-   line3_level2_data = []
-   for zz in d:
-       line3_level2_data.append(ord(zz))        
-   line =  "1,1,1,1,1"                                 
-   d = line.split(",")
+   line2_level2_data = []
+   readCSVLines(line_boxs, line2_level2_data, line_rotations, line2_level2_rots)  
+
+   #pallet_data[level=1][0][row=2] = "B,B,B,C,C,F" 
+   line_boxs =  "B,B,B,C,C,F"                                 
+   #pallet_data[level=1][1][row=2] = "1,1,1,1,1"    
+   line_rotations =    "1,1,1,1,1"                                   
    line3_level2_rots = []
-   for zz in d:
-       line3_level2_rots.append(zz) 
+   line3_level2_data = []
+   readCSVLines(line_boxs, line3_level2_data, line_rotations, line3_level2_rots)
+   
+if recipe_id == (7 | 10<<8 | 10<<16):                    # 7 A, 10 B, 10 C
+
+   # pallet_data[level=0][0][row=0] = "A,A,A,A,E" is bottom level first row
+   # pallet_data[level=0][1][row=0] = "0,0,0,0"                 # rotations for that bottom level first row
+   line_boxs =  "A,A,A,A,E"                                     # bottom level first row the csv data is either from a recipe file or SAP ?
+   line_rotations =  "0,0,0,0"                                  # bottom level first row
+   line1_level1_rots = []
+   line1_level1_data = []
+   readCSVLines(line_boxs, line1_level1_data, line_rotations, line1_level1_rots)
+   # pallet_data[level=0][1][row=0] = "0,0,0,0"                   # rotations for that bottom level first row
+ 
+   #pallet_data[level=0][0][row=1] = "B,B,C,C,C,E"  
+   line_boxs =  "B,B,C,C,C,E"                                  
+   # pallet_data[level=0][1][row=1] = "0,0,0,1,1"   
+   line_rotations =  "0,0,0,1,1"                              
+   line2_level1_rots = []
+   line2_level1_data = []
+   readCSVLines(line_boxs, line2_level1_data, line_rotations, line2_level1_rots)
+   
+   #pallet_data[level=0][0][row=2] = "B,B,B,C,C,L" 
+   line_boxs =  "B,B,B,C,C,L"                                 
+   #pallet_data[level=0][1][row=2] = "1,1,1,1,1"   
+   line_rotations =  "1,1,1,1,1"                                 
+   line3_level1_rots = []
+   line3_level1_data = []
+   readCSVLines(line_boxs, line3_level1_data, line_rotations, line3_level1_rots)
+   
+   #   pallet_data[level=1][0][row=0] = "A,A,A,A,E" 
+   line_boxs =  "A,A,A,A,E"                                 
+   #    pallet_data[level=1][1][row=0] = "0,0,0,0"   
+   line_rotations =   "0,0,0,0"                                  
+   line1_level2_rots = []
+   line1_level2_data = []
+   readCSVLines(line_boxs, line1_level2_data, line_rotations, line1_level2_rots)
+   
+   # pallet_data[level=1][0][row=1] = "B,B,C,C,C,E"
+   line_boxs =  "B,B,C,C,C,E"                                 
+   # pallet_data[level=1][1][row=1] = "0,0,0,1,1"    
+   line_rotations =   "0,0,0,1,1"                                   
+   line2_level2_rots = []
+   line2_level2_data = []
+   readCSVLines(line_boxs, line2_level2_data, line_rotations, line2_level2_rots)  
+
+   #pallet_data[level=1][0][row=2] = "B,B,B,C,C,F" 
+   line_boxs =  "B,B,B,C,C,F"                                 
+   #pallet_data[level=1][1][row=2] = "1,1,1,1,1"    
+   line_rotations =    "1,1,1,1,1"                                   
+   line3_level2_rots = []
+   line3_level2_data = []
+   readCSVLines(line_boxs, line3_level2_data, line_rotations, line3_level2_rots)
 
 # make recipe array(s) from our data set chosen from the given recipe above it comprises of 3 rows(box and rotation) and 2 stack levels
 palet_data_lev1 = np.array([[line1_level1_data,
@@ -367,19 +337,38 @@ palet_wid = 0
 # level is the stack row is the horizontal line box_no is the number in the sequence place is set when the box was found and picked up
 # for us to then place it.
 #
-# we have a manual entry here if we want to start the sequence mid way (return from error)
+# we have a manual entry here if we want to start the sequence mid way (return from an error)
 #
-if len(sys.argv) >= 3:                             # passed a manual pickup point to start from via the arguments
+if len(sys.argv) >= 13:                             # passed a manual pickup point to start from via the arguments
     level = int(sys.argv[1])
     row = int(sys.argv[2])
     box_no = int(sys.argv[3])
-else:                                            # start at the begining
+    box_a_cnt = int(sys.argv[4])                    # re-start the box counters with what we have already in the pallet
+    box_b_cnt = int(sys.argv[5])
+    box_c_cnt = int(sys.argv[6])
+    box_d_cnt = int(sys.argv[7])
+    box_g_cnt = int(sys.argv[8])
+    box_h_cnt = int(sys.argv[9])
+    box_i_cnt = int(sys.argv[10])
+    box_j_cnt = int(sys.argv[11])    
+    box_k_cnt = int(sys.argv[12]) 
+    if sys.argv[0] >= 14:    
+        place = int(sys.argv[13])                   # start palacing rather than picking if set to non-zero
+else:                                               # start at the begining
     level = 0
     row = 0
     box_no = 0
-
-# default the place flag until the box was identified and picked up    
-place = 0
+    box_a_cnt = 0
+    box_b_cnt = 0
+    box_c_cnt = 0
+    box_d_cnt = 0
+    box_g_cnt = 0
+    box_h_cnt = 0
+    box_i_cnt = 0
+    box_j_cnt = 0    
+    box_k_cnt = 0      
+    # default the place flag until the box was identified and picked up    
+    place = 0
 
 # data organisation layout for 2d array(s) representing each row
 box = 0                  # first the boxes
@@ -387,15 +376,20 @@ rot = 1                  # next the rotations
 
 # connect to UR robot
 soc = robot_connect("192.168.250.112", 30004)
-    
+
+# perfrom the palletization operation until we reach the box label F = Finish
+exit_scan = 0   
 while True:
 
+    # --------- scan for box and pick it --------------
     for s in scan_loc:
+        if exit_scan == 1:
+            break
         move_to_loc(s[0],s[1],soc)                                       # advance to each location (bay) where boxs are stored  
         sel_lvl = pallet_levls[level]                                    # select the correct numpy array for the stack level
         box_match = false
         box_to_find = sel_lvl[row][box][box_no]                          # get next box requirement        
-        if box_to_find == ord("E"):                                      # denotes new row
+        if box_to_find == ord("E"):                                      # denotes new row increment the row index and reset the total width on that row
             row += 1
             palet_wid = 0
         elif box_to_find == ord("L"):                                    # denotes next level
@@ -407,28 +401,51 @@ while True:
             box_no += 1
         elif box_to_find == ord("F"):                                    # denotes pallet complete
             print("palletising complete.....")
+            print("box A : ",box_a_cnt,"box B : ",box_b_cnt,"box C : ",box_c_cnt,"box D : ",box_d_cnt)
+            print("box G : ",box_g_cnt,"box H : ",box_h_cnt,"box I : ",box_i_cnt,"box J : ",box_j_cnt,"box K : ",box_k_cnt)
             # disconnect UR robot
             robot_disconnect(soc) 
             sys.exit(0)
-        else:                                                            # should be a valid box
-            box_match = check_box_with_cam(chr(box_to_find), exp_dates[find_index[chr(box_to_find), boxs]])   # look for it in stock
-            if box_match == True:
-                box_width = get_box_widths(chr(box_to_find))             # get the box types width from the SAP data
-                if (grab_box(box_width, soc) == True):                       # succesfully grabbed the box
-                    place = 1                                            # set to place it
-                    palet_wid += box_width
-                    break
+        else:                                                                # should be a valid box
+            while True:                                                      # keep grabbing box until done
+                box_match = check_box_with_cam(chr(box_to_find), exp_dates[find_index[chr(box_to_find), boxs]])   # look for it in stock
+                if box_match == True:
+                    box_width = get_box_widths(chr(box_to_find))             # get the box types width from the SAP data
+                    if (grab_box(box_width, soc) == True):                   # succesfully grabbed the box
+                        place = 1                                            # set to place it
+                        palet_wid += box_width
+                        exit_scan = 1
+                        break                                                # exit and place it
+                    else:
+                        print("Failed to grab box")
+                        # sys.exit(-1)
                 else:
-                    print("Failed to grab box")
-                    # sys.exit(-1)
-            else:
-                print("wrong box or wrong date @loc ",s[0]," ",s[1])
-                    
+                    print("wrong box or wrong date @loc ",s[0]," ",s[1])
+
+    # ------------ move to palet and place box --------------                    
     while (place == 1) :                                                 # now place the box loop on error
         move_to_loc(palet_loc[0], palet_loc[1], soc)                     # move to pallet defined location
         if (place_box(add_3coords(palet_pos_offset+(palet_row_offset*row)+get_offset_from_width(palet_wid), level), sel_lvl[row][rot][box_no], soc) == True):
             box_no += 1
-            place = 0
-
+            place = 0 
+            # increment the count of boxes for the specified box placed
+            if box_to_find == ord("A"):                                    
+                box_a_cnt += 1        
+            elif box_to_find == ord("B"):                                    
+                box_b_cnt += 1 
+            elif box_to_find == ord("C"):                                    
+                box_c_cnt += 1 
+            elif box_to_find == ord("D"):
+                box_d_cnt += 1 
+            elif box_to_find == ord("G"):
+                box_g_cnt += 1 
+            elif box_to_find == ord("H"):
+                box_h_cnt += 1 
+            elif box_to_find == ord("I"):
+                box_i_cnt += 1 
+            elif box_to_find == ord("J"):
+                box_j_cnt += 1 
+            elif box_to_find == ord("K"):
+                box_k_cnt += 1 
 # disconnect UR robot
 robot_disconnect(soc)   
