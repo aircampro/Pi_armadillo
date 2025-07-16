7309a79d3eb81187753e19730fb905b9c60d52e4
aircampro <147149143+aircampro@users.noreply.github.com>
2025-07-16 16:45:42 +0100

    Add files via upload

---
+++ hardware/dht11_adafruit.py
@@ -0,0 +1,13 @@
+# DHT11 using Adafruit library https://github.com/adafruit/Adafruit_Python_DHT
+#
+import Adafruit_DHT
+# GPIO 
+DHT_GPIO = 18
+# make DHT11 object
+sensor = Adafruit_DHT.DHT11
+MAX_TRIAL = 3
+for _ in range(self.MAX_TRIAL):
+  humidity, temperature =  Adafruit_DHT.read_retry(sensor, DHT_GPIO)
+  if humidity is None or temperature is None:
+    continue
+  return [humidity, temperature]
