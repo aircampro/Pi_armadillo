a5bf8a4601025867d7d5e8e74801941251f1cfca
aircampro <147149143+aircampro@users.noreply.github.com>
2025-08-06 16:47:54 +0100

    Add files via upload

--- plc_robot_coms/hesai_lidar_example.cc
+++ plc_robot_coms/hesai_lidar_example.cc
@@ -20,13 +20,46 @@
  *****************************************************************************/
 
 #include "pandarGeneral_sdk/pandarGeneral_sdk.h"
+#include <iostream>
+#include <csignal>
+#ifdef __linux__ 
+    #include <signal.h>
+#elif _WIN32
+    #include <windows.h>
+#else
+
+#endif
+
+volatile int g_a = 0;
+
+#ifdef __linux__ 
+void sig_handler(int signo) 
+{
+  std::cout << "received signal: " << signo << "\n";
+  g_a = 1;
+}
+#elif _WIN32
+BOOL WINAPI consoleCtrlHandler(DWORD ctrlType) {
+   switch (ctrlType) {
+      case CTRL_C_EVENT:
+      std::cout << "Ctrl+C signal received." << std::endl;
+	  g_a = 1;
+      return TRUE;
+      case CTRL_BREAK_EVENT:
+      std::cout << "Ctrl+Break signal received." << std::endl;
+	  g_a = 1;
+      return TRUE;
+      default:
+      return FALSE;
+    }
+}
+#else
+
+#endif
 
 void gpsCallback(int timestamp) {
   printf("gps: %d", timestamp);
 }
-
-std::cout << "sensor origin (xyz): [" << p.sensor_origin_.x () << ", " << p.sensor_origin_.y () << ", " << p.sensor_origin_.z () << "]" >> std::endl; 
-std::cout << "orientation (xyzw): [" << p.sensor_orientation_.x () << ", " << p.sensor_orientation_.y () << ", " << p.sensor_orientation_.z () << ", " << p.sensor_orientation_.w () << "]" << std::endl;
 
 void lidarCallback(boost::shared_ptr<PPointCloud> cld, double timestamp) {
   printf("Frame timestamp: %lf,\n", timestamp);
@@ -41,7 +74,44 @@ void lidarCallback(boost::shared_ptr<PPointCloud> cld, double timestamp) {
 int main(int argc, char** argv) {
   // PandarGeneralSDK pandarGeneral(std::string("192.168.1.201"), 2368, 10110, \
   //     lidarCallback, gpsCallback, 0, 0, 1, std::string("Pandar40P"));
+  /* example panda40
+      HesaiLidarSDK pandarGeneral(
+	8080				// lidar data port //, 
+	8308				// gps data port //, 
+        0                               // start angle//,
+	std::string("correction.csv")	// calibration file of lidar //, 
+	lidarCallback 			// point cloud data call back //, 
+	gpsCallback 			// gps data callback //, 
+	HESAI_LIDAR_RAW_DATA_STRCUT_SINGLE_RETURN// Return Mode: Single Return data structure //, 
+	40				// laser counter //, 
+	HESAI_LIDAR_PCL_DATA_TYPE_REDUCED// pcl data alignment //
+	);
+   */
+   /* pandora
+     HesaiLidarSDK pandarGeneral(
+    std::string("192.168.20.51"), 
+    9870, 
+    std::string("calibration.yml"), 
+    cameraCallback,
+    2368, 10110, 0,
+    std::string("correction.csv"),
+    lidarCallback, gpsCallback,
+    HESAI_LIDAR_RAW_DATA_STRCUT_DUAL_RETURN, 40, HESAI_LIDAR_PCL_DATA_TYPE_REDUCED);
+	*/
+   
+#ifdef __linux__ 
+  signal(SIGUSR1, sig_handler);
+  signal(SIGUSR2, sig_handler);
+  signal(SIGINT, sig_handler);
+#elif _WIN32  
+  if (!SetConsoleCtrlHandler(consoleCtrlHandler, TRUE)) {
+      std::cerr << "Error setting console control handler." << std::endl;
+      return 1;
+  }
+#else
 
+#endif
+	
   PandarGeneralSDK pandarGeneral(std::string("/path/to/pcapFile"), \
   lidarCallback, 0, 0, 1, std::string("PandarXT-16"));
   std::string filePath = "/path/to/correctionFile";
@@ -59,7 +129,7 @@ int main(int argc, char** argv) {
 
   pandarGeneral.Start();
 
-  while (true) {
+  while (g_a == 0) {
     sleep(100);
   }
   
