8345666987794cb850d1a72461c0e98baeffcc3e
aircampro <147149143+aircampro@users.noreply.github.com>
2025-09-04 12:29:48 +0100

    Add files via upload

---
+++ video/optris_thermal_image.py
@@ -0,0 +1,69 @@
+#
+# Optris thermal imaging camera connected on usb
+# pythonic interface to the Evocortex libirimager
+# ref:- https://github.com/FiloCara/pyOptris?tab=readme-ov-file
+#
+import pyOptris as optris
+import sys
+import cv2
+
+# choose relevant library
+if not sys.platform.find("linux") == -1:
+    LIB_path = "/usr/lib/libirdirectsdk.so"
+elif not sys.platform.find("win32") == -1:
+    LIB_path = "../irDirectSDK/sdk/x64/libirimager.dll"
+else:
+    print("unsupported os")
+    sys.exit()
+
+# temperature range
+TMIN = 300
+TMAX = 900
+
+if __name__ == "__main__":
+
+    # load library
+    optris.load_DLL(LIB_path)
+    # USB connection initialisation
+    optris.usb_init('config_file.xml')
+
+    w, h = optris.get_thermal_image_size()
+    print('{} x {}'.format(w, h))
+
+    # set up the camera 
+	if optris.set_shutter_mode(optris.ShutterMode.AUTO) == 0:
+        if optris.set_palette.(optris.ColouringPalette.IRON_HI) == 0:
+            if optris.set_palette_scale(optris.PaletteScalingMethod.SIGMA3) == 0:
+			    if optris.set_temperature_range(TMIN, TMAX) == 0:
+                    emissivity, transmissivity, ambientTemperature = 0.9, 0.5, 20.0
+                    if optris.set_radiation_parameters(emissivity, transmissivity, ambientTemperature) == 0:
+                        print("optris camera set-up correctly")
+	
+    # Get the thermal frame as (numpy array)
+    thermal_frame = optris.get_thermal_image(w, h)
+    # Conversion to temperature values are to be performed as follows:
+    # t = ((double)data[x] - 1000.0) / 10.0;
+    processed_thermal_frame = (thermal_frame - 1000.0) / 10.0 
+    print("processed frame : ", processed_thermal_frame)
+
+    optris.set_palette(9)
+    w, h = optris.get_palette_image_size()
+    print("{} x {}".format(w, h))
+    while True:
+        # Get the palette image (RGB image) and display it
+        frame = optris.get_palette_image(w, h)
+        cv2.imshow("IR streaming", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        elif cv2.waitKey(1( & 0xFF == ord("f"):
+            f = optris.get_focus_motor_position()
+            f += 0.1
+            if not optris.set_focus_motor_position(f) == 0:
+                print("error setting focus")
+        elif cv2.waitKey(1( & 0xFF == ord("b"):
+            f = optris.get_focus_motor_position()
+            f -= 0.1
+            if not optris.set_focus_motor_position(f) == 0:
+                print("error setting focus")
+    optris.terminate()
+    cv2.destroyAllWindows()
