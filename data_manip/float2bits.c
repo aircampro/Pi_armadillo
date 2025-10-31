0998d7d81b0bc343d9207a9936ecc1be1ad50a4a
aircampro <147149143+aircampro@users.noreply.github.com>
2025-10-31 13:34:20 +0000

    Update float2bits.c
    added int back to float with pointer method

--- data_manip/float2bits.c
+++ data_manip/float2bits.c
@@ -4,13 +4,17 @@
 int main() {
     // float to int using typecast pointer
     float f = -12.5f;
+    // to convert
     int i = *( ( int* )&f );
     printf( "%f ( %08X )\n", f, i );
     for( int j = 31; j >= 0; j-- ){
         printf( "%d", ( i >> j ) & 1 );
     }
     printf( "\n" );
-
+    // and back
+    float z = *( ( float* )&i );
+    printf( "%f\n", z );
+    
     // float to int using union
     union { float f; int i; } a;
     a.f = -2.5f;
