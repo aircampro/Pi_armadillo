8bc6d532a1894b41f2c32d6d09201f2587e41439
aircampro <147149143+aircampro@users.noreply.github.com>
2025-12-17 23:45:13 +0000

    Add files via upload

---
+++ ML/manga_ocr.py
@@ -0,0 +1,24 @@
+#
+# simple call to this class to translate japanese manga https://github.com/kha-white/manga-ocr
+#
+#
+import sys
+from PIL import Image
+from PIL import UnidentifiedImageError
+from manga_ocr import MangaOcr
+
+def do_ocr(img_or_path):
+    mocr = MangaOcr()
+    img = Image.open(path)
+    text = mocr(img_or_path)
+    print(text)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) >= 1:
+        filen = str(sys.argv[1])
+    else:
+        print("please pass file name first")
+    do_ocr(filen)
+
+
