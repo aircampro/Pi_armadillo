9d165b7a7f0a409813b7d8c0b080f13f5538b948
aircampro <147149143+aircampro@users.noreply.github.com>
2026-01-09 12:53:12 +0000

    Update email_rw_class.py
    added SSL

--- iot_enocean/email_rw_class.py
+++ iot_enocean/email_rw_class.py
@@ -33,11 +33,10 @@ class GmMailer:
 
     self.password = pas
     self.addr_from = adr_from
-	
     self.addr_to = addr_to
     self.subject = subject
     self.body = body
-  
+
   def send(self):
     """ sends email
     """
@@ -151,7 +150,7 @@ def check_from_email(email_id, passwrd, from_this_mail):
                         c += 1
         mygmail.close()
     return c
-	
+
 def count_email(email_id, passwrd):
     c=0
     mygmail = GmReceiver()
@@ -163,26 +162,100 @@ def count_email(email_id, passwrd):
                     c += 1
         mygmail.close()
     return c
-	
+
+# using SSL for SMTP
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from typing import Any
+
+from configparser import ConfigParser, MissingSectionHeaderError
+from pathlib import Path
+import os
+
+class Conf(ConfigParser):
+
+	def __init__(self, confFile=None, data=None, section=None):
+		super().__init__(strict=False)
+		self.confFile = confFile
+		if isinstance(data, dict):
+			self.read_dict({section:data} if section else data)
+		else:
+			if data is None:
+				assert confFile
+				data = Path(confFile).read_text()
+			if not section:
+				try:
+					self.read_string(data)
+				except MissingSectionHeaderError:
+					assert confFile
+					section = os.path.splitext(os.path.basename(confFile))[0]
+			if section:
+				self.read_string("[{}]\n{}".format(section, data))
+
+	#The default converts the name to lowercase. Override to keep the key as they are.
+	def optionxform(self, optionstr):
+		return optionstr
+
+	def update(self, data:dict[str, dict[str, Any]]):
+		for section, options in data.items():
+			if not self.has_section(section):
+				self.add_section(section)
+			for option, value in options.items():
+				self[section][option] = str(value)
+
+	def save(self, section=None):
+		assert self.confFile
+		with open(self.confFile, 'w') as f:
+			if section:
+				for k,v in self.items(section):
+					f.write("{}={}\n".format(k,v))
+			else:
+				self.write(f)
+
+	def dict(self, section=None):
+		if section:
+			return dict(self.items(section))
+		else:
+			return {s:dict(self.items(s)) for s in self.sections()}
+smtpConfFile = Path('/etc/smtp.conf')
+def sendEmail(msg):
+	conf = Conf(smtpConfFile)
+	SMTP = smtplib.SMTP_SSL if conf.getboolean('smtp', 'ssl', fallback=False) else smtplib.SMTP
+	with SMTP(conf.get('smtp','host'), conf.getint('smtp','port', fallback=0), timeout=5) as smtp:
+		try:
+			smtp.starttls()
+		except:pass
+
+		if conf.get('smtp', 'user', fallback=False):
+			smtp.login(conf.get('smtp', 'user'), conf.get('smtp', 'pass'))
+
+		if msg:
+			smtp.send_message(msg, from_addr='noreply@local')
+		else:
+			smtp.noop()
+
 if __name__ =="__main__":
     email_id = os.getenv('EMAIL')                                             # read from env settings 
     passw = os.getenv('PASSWORD')
-    addr_to = "xx@gmail.com"
-    # show how to read emails 
-    tot_num_of_em = count_email(email_id, passw)
+	addr_to = "xx@gmail.com"
+	# show how to read emails 
+	tot_num_of_em = count_email(email_id, passw)
     print(f"you have {tot_num_of_em} emails")
     read_email(email_id, passw)
     # count the number of emails already in box from the reciever
-    num_of_em = check_from_email(email_id, passw, addr_to)
-    # now send a test email 
+	num_of_em = check_from_email(email_id, passw, addr_to)
+	# now send a test email 
     subject = "Send Test e-mail"
     body = create_mail_body( "Mark Nicholas", "40.3" )
     GmMailer = GmMailer(email_id, passw, addr_to, subject, body)
     GmMailer.send()
     new_num_of_em = 0
-    # now wait for confirmation email from the reciever
+	# now wait for confirmation email from the reciever
     while not (num_of_em == new_num_of_em):
         new_num_of_em = check_from_email(email_id, passw, addr_to) 
-        time.sleep(1)
+		time.sleep(1)
     print("completed! - we received confirmation back from receiver ",addr_to)
-	
+    # using smtp with SSL TLSーSMTP
+    msg1 = " This is the SSL test"
+    sendEmail(msg)	
