eedbe9db5132c697b56845c1c4dd86a1d3797d35
aircampro <147149143+aircampro@users.noreply.github.com>
2026-01-12 15:18:31 +0000

    Add files via upload

---
+++ video/RTSP_Client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# ffplay rtsp://{username}:{password}@{camera_ip}:{port}/{path}
+# ffprobe rtsp://username:password@camera_ip:port/path
+# RTSP Client
+#
+import cv2
+
+# RTSP URL 
+username="root"
+password="pass001"
+camera_ip="10.0.5.6"
+
+if len(sys.argv) >= 1:
+    rtsp_url = sys.argv[1]
+else:
+    rtsp_url = f"rtsp://{username}:{password}@{camera_ip}"
+
+cap = cv2.VideoCapture(rtsp_url)
+
+if not cap.isOpened():
+    print("RTSP stream would not open closing..")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("cant get frame.. exiting")
+        break
+
+    cv2.imshow("RTSP Stream", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
