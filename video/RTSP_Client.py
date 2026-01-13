6482d4a6ecadda958e5d3807b76cc358d82fad68
aircampro <147149143+aircampro@users.noreply.github.com>
2026-01-13 15:46:14 +0000

    Add files via upload

--- video/RTSP_Client.py
+++ video/RTSP_Client.py
@@ -10,6 +10,15 @@ username="root"
 password="pass001"
 camera_ip="10.0.5.6"
 
+# common RTSP links for various manufacturers camera_ip
+VID=0
+PIC=1
+channel_id=0
+links = {'hikvision': ['Streaming/Channels/{channel_id}/', 'ISAPI/Streaming/channels/{channel_id}/picture'], 
+'axis': ['axis-media/media.amp', 'axis-cgi/jpg/image.cgi'],
+'rasppi': ['', 'snap?action=snapshot']}
+# e.g. rtsp_url = links['hikvision'][VID]
+
 if len(sys.argv) >= 1:
     rtsp_url = sys.argv[1]
 else:
