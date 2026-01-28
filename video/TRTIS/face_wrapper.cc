7062687509d880c07386b22cbf8176bad00e53b1
aircampro <147149143+aircampro@users.noreply.github.com>
2026-01-28 12:15:46 +0000

    Add files via upload

---
+++ video/TRTIS/face_wrapper.cc
@@ -0,0 +1,268 @@
+/*
+
+filename :- face_wrapper.cc
+ref :- https://qiita.com/shotasakamoto/items/bbe9bcf665715606de9d
+    https://github.com/sakamomo554101/sample_trtis/blob/master/src/custom_backend/face_recognition_model/face_wrapper.cc
+
+changes
+Allow the path of a dataset to be changed from config
+enable to set the path of model
+
+*/
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+// custom header
+#include "custom_backend/face_recognition_model/face_wrapper.h"
+
+// dlib
+#include <dlib/dnn.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing.h>
+#include <dlib/opencv.h>
+using namespace dlib;
+
+namespace nvidia { namespace inferenceserver { namespace custom {
+namespace face_recognition_model {
+
+FaceWrapper::FaceWrapper() {
+}
+
+FaceWrapper::~FaceWrapper() {
+}
+
+int
+FaceWrapper::Initialize(float distance_threshold, std::string pth_mdl="/models/face_recognition_model/dlib_face_recognition_resnet_model_v1.dat", pth_sp="/models/face_recognition_model/shape_predictor_5_face_landmarks.dat", std::string ds1="/dataset/face/face.csv", std::string ds2="/dataset/face/image/" ) {
+  // set parameter
+  face_detector = get_frontal_face_detector();
+  this->distance_threshold = distance_threshold;
+
+  // model datas are copied on models folder (TODO : enable to set the path of model)
+  deserialize(pth_mdl) >> net;
+  deserialize(pth_sp) >> sp;
+
+  // register face database (TODO Allow the path of a dataset to be changed from config
+  int register_count = registerFaceDatasFromCsv(ds1, ds2);
+  std::cout << LOG_PREFIX << " initialize : register face data count is " << register_count << std::endl;
+
+  // print DLIB_USE_CUDA if needed
+  if (kDebugMode) {
+#ifdef DLIB_USE_CUDA
+    std::cout << LOG_PREFIX << " DLIB mode is GPU" << std::endl;
+#else
+    std::cout << LOG_PREFIX << " DLIB mode is CPU" << std::endl;
+#endif
+  }
+
+  return ErrorCodes::Success;
+}
+
+std::vector<face_recognition_model::FaceData>
+FaceWrapper::ExtractFaceData(cv::Mat cv_img)
+{
+  std::vector<face_recognition_model::FaceData> face_datas;
+
+  // convert dlib image data from opencv image data
+  auto dlib_cv_img = dlib::cv_image<dlib::rgb_pixel>(cv_img);
+
+  // extract face rects
+  std::vector<dlib::rectangle> dets = extractFaceRect(dlib_cv_img);
+
+  // check count of dets
+  if (dets.empty()) {
+    std::cout << LOG_PREFIX << " face data is not found" << std::endl;
+    return face_datas;
+  }
+
+  // create face datas
+  auto face_datas_in_db = GetFaceDatasFromDB();
+  for (auto det : dets) {
+    // vectorize the face data
+    auto face_descriptor = calcFaceDescriptor(dlib_cv_img, det);
+
+    // judge the similar face
+    auto face_name = predictSimilarFace(face_descriptor, face_datas_in_db, distance_threshold);
+
+    // create face data
+    face_recognition_model::FaceData data(det, face_descriptor);
+    data.SetName(face_name);
+    face_datas.push_back(data);
+  }
+
+  return face_datas;
+}
+
+std::vector<face_recognition_model::FaceData>
+FaceWrapper::GetFaceDatasFromDB()
+{
+  return face_db;
+}
+
+std::vector<dlib::rectangle> 
+FaceWrapper::extractFaceRect(dlib::cv_image<dlib::rgb_pixel> dlib_cv_img)
+{
+  return face_detector(dlib_cv_img);
+}
+
+matrix<rgb_pixel>
+FaceWrapper::extractFaceChip(dlib::cv_image<dlib::rgb_pixel> dlib_cv_img, dlib::rectangle face_rect)
+{
+  auto shape = sp(dlib_cv_img, face_rect);
+  matrix<rgb_pixel> face_chip;
+  extract_image_chip(dlib_cv_img, get_face_chip_details(shape,150,0.25), face_chip);
+  return face_chip;
+}
+
+matrix<float,0,1> 
+FaceWrapper::calcFaceDescriptor(dlib::cv_image<dlib::rgb_pixel> dlib_cv_img, dlib::rectangle face_rect)
+{
+  // extract face chips (ex. nose, mouth..)
+  auto face_chip = extractFaceChip(dlib_cv_img, face_rect);
+  std::vector<matrix<rgb_pixel>> faces;
+  faces.push_back(face_chip);
+
+  // vectorize the face data 
+  std::vector<matrix<float,0,1>> face_descriptors = net(faces);
+  return face_descriptors[0];
+}
+
+std::string 
+FaceWrapper::predictSimilarFace(matrix<float,0,1> target_descriptor, std::vector<face_recognition_model::FaceData> reference_face_datas, float distance_threshold)
+{
+  if (reference_face_datas.size() == 0) {
+    return FACE_TYPE_UNKNOWN;
+  }
+
+  // TODO : Set a threshold of face distance and adopt the nearest face data among them (it is better to use a classifier such as SVM in the future)
+  // calculate the face distance between target face data and face datas of database
+  std::map<float, std::string> face_distance_infos;
+  for (auto ref_face_data : reference_face_datas) {
+    float distance = length(target_descriptor - ref_face_data.GetFaceDescriptor());
+    face_distance_infos[distance] = ref_face_data.Name();
+  }
+
+  // check threshold
+  if (face_distance_infos.begin()->first < distance_threshold) {
+    return face_distance_infos.begin()->second;
+  }
+  return FACE_TYPE_UNKNOWN;
+}
+
+int 
+FaceWrapper::registerFaceDatas(std::vector<cv::Mat> cv_imgs, std::vector<std::string> face_names)
+{
+  // error check
+  if (cv_imgs.size() != face_names.size()) {
+    return kFaceRegisterError;
+  }
+
+  // get face vector
+  for (std::size_t i = 0; i < cv_imgs.size(); i++) {
+    // create and register face data
+    face_recognition_model::FaceData data;
+    auto error_code = createFaceData(cv_imgs[i], face_names[i], data);
+    if (error_code != ErrorCodes::Success) {
+      continue;
+    }
+    face_db.push_back(data);
+  }
+  return ErrorCodes::Success;
+}
+
+int
+FaceWrapper::createFaceData(cv::Mat cv_img, std::string face_name, face_recognition_model::FaceData& face_data)
+{
+  auto dlib_cv_img = dlib::cv_image<dlib::rgb_pixel>(cv_img);
+  std::vector<dlib::rectangle> dets = extractFaceRect(dlib_cv_img);
+
+  // check error
+  if (dets.empty()) {
+    // if face rect is not found, this face data is ignored.
+    std::cout << LOG_PREFIX << " face data is not found : " << face_name << std::endl;
+    return kFaceNotFound;
+  } else if (dets.size() > 1) {
+    // if multi face data is detected in one image data, this face datas are ignored.
+    std::cout << LOG_PREFIX << " multi face datas are detected! this image is ignored : " << face_name << std::endl;
+    return kFaceMultiple;
+  }
+
+  // vectorize the face data
+  dlib::rectangle det = dets[0];
+  auto face_descriptor = calcFaceDescriptor(dlib_cv_img, det);
+
+  // create face data
+  face_recognition_model::FaceData data(det, face_descriptor);
+  data.SetName(face_name);
+  face_data = data;
+  return ErrorCodes::Success;
+}
+
+int 
+FaceWrapper::registerFaceDatasFromCsv(std::string csv_path, std::string root_folder_path)
+{
+  int register_count = 0;
+  std::ifstream ifs(csv_path);
+  std::string line;
+  while (getline(ifs, line)) {
+    auto texts = split_text(line, ',');
+
+    // texts have the following contents (each line has 2 columns)
+    // [0] -> image file name
+    // [1] -> face name
+    if (texts.size() != 2) {
+      // TODO : display error log
+      continue;
+    }
+    std::string csv_file_name = texts[0];
+    std::string face_name = texts[1];
+
+    // create opencv matrix
+    auto path = root_folder_path + csv_file_name;
+    cv::Mat cv_img = cv::imread(path, 1);
+
+    // create face data
+    face_recognition_model::FaceData data;
+    auto error_code = createFaceData(cv_img, face_name, data);
+
+    // register face data if successed
+    if (error_code != ErrorCodes::Success) {
+      // TODO : display error log
+      continue;
+    }
+    face_db.push_back(data);
+    register_count++;
+  }
+
+  return register_count;
+}
+
+std::vector<std::string> 
+FaceWrapper::split_text(const std::string s, char delim)
+{
+  std::vector<std::string> elems;
+  std::string item;
+  for (char ch: s) {
+      if (ch == delim) {
+          if (!item.empty())
+              elems.push_back(item);
+          item.clear();
+      }
+      else {
+          item += ch;
+      }
+  }
+  if (!item.empty())
+      elems.push_back(item);
+  return elems;
+}
+
+}  // namespace face_recognition_model
+}}}  // namespace nvidia::inferenceserver::custom
---
+++ video/TRTIS/face_wrapper.h
@@ -0,0 +1,162 @@
+/*
+ file :- face_wrapper.h
+ ref:- https://github.com/sakamomo554101/sample_trtis/blob/master/src/custom_backend/face_recognition_model/face_wrapper.h
+ changes
+ Allow the path of a dataset to be changed from config
+ enable to set the path of model
+*/
+
+#include <iostream>
+// trtis
+#include "src/custom/sdk/custom_instance.h"
+
+// opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+
+// dlib
+#include <dlib/dnn.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing.h>
+#include <dlib/opencv.h>
+using namespace dlib;
+
+// The next bit of code defines a ResNet network.  It's basically copied
+// and pasted from the dnn_imagenet_ex.cpp example, except we replaced the loss
+// layer with loss_metric and made the network somewhat smaller.  Go read the introductory
+// dlib DNN examples to learn what all this stuff means.
+//
+// Also, the dnn_metric_learning_on_images_ex.cpp example shows how to train this network.
+// The dlib_face_recognition_resnet_model_v1 model used by this example was trained using
+// essentially the code shown in dnn_metric_learning_on_images_ex.cpp except the
+// mini-batches were made larger (35x15 instead of 5x5), the iterations without progress
+// was set to 10000, and the training dataset consisted of about 3 million images instead of
+// 55.  Also, the input layer was locked to images of size 150.
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET> 
+using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
+
+template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
+template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
+
+template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
+template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
+template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
+template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
+template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
+
+using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
+                            alevel0<
+                            alevel1<
+                            alevel2<
+                            alevel3<
+                            alevel4<
+                            max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
+                            input_rgb_image_sized<150>
+                            >>>>>>>>>>>>;
+
+#define FACE_TYPE_UNKNOWN "unknown"
+
+namespace nvidia { namespace inferenceserver { namespace custom {
+namespace face_recognition_model {
+
+// this class contains the face area and name
+class FaceData {
+  public:
+    FaceData()
+    {
+      // TODO : this constructor should be removed bacause some values of this class are not initialized.
+      this->name = FACE_TYPE_UNKNOWN;
+    }
+    FaceData(const dlib::rectangle rect, const matrix<float,0,1> face_descriptor) 
+    {
+      this->rect = rect;
+      this->face_descriptor = face_descriptor;
+      this->name = FACE_TYPE_UNKNOWN;
+    }
+    ~FaceData() {}
+
+    dlib::rectangle Rect()
+    {
+      return this->rect;
+    }
+
+    std::string Name()
+    {
+      return this->name;
+    }
+
+    void SetName(std::string name)
+    {
+      this->name = name;
+    }
+
+    matrix<float,0,1> GetFaceDescriptor()
+    {
+      return this->face_descriptor;
+    }
+
+  private:
+    dlib::rectangle rect;
+    matrix<float,0,1> face_descriptor;
+    std::string name;
+};
+
+class FaceWrapper {
+  public:
+    FaceWrapper();
+    ~FaceWrapper();
+    int Initialize(float distance_threshold = 0.6, std::string pth_mdl="/models/face_recognition_model/dlib_face_recognition_resnet_model_v1.dat", pth_sp="/models/face_recognition_model/shape_predictor_5_face_landmarks.dat", std::string ds1="/dataset/face/face.csv", std::string ds2="/dataset/face/image/" );
+
+    std::vector<face_recognition_model::FaceData> ExtractFaceData(cv::Mat cv_img);
+    std::vector<face_recognition_model::FaceData> GetFaceDatasFromDB();
+
+    // error codes
+    const int kFaceRegisterError = -1;
+    const int kFaceNotFound = -2;
+    const int kFaceMultiple = -3;
+
+  private:
+    const std::string LOG_PREFIX = "[FaceWrapper]";
+
+    // face detector
+    dlib::frontal_face_detector face_detector;
+
+    // shape predictor
+    dlib::shape_predictor sp;
+
+    // vectorizer of face data
+    anet_type net;
+
+    // face distance threshold
+    float distance_threshold;
+
+    // TODO : データベースは別で作った方が良い
+    std::vector<face_recognition_model::FaceData> face_db;
+
+    // private functions
+    std::vector<dlib::rectangle> extractFaceRect(dlib::cv_image<dlib::rgb_pixel> dlib_cv_img);
+    matrix<rgb_pixel> extractFaceChip(dlib::cv_image<dlib::rgb_pixel> dlib_cv_img, dlib::rectangle face_rect);
+    matrix<float,0,1> calcFaceDescriptor(dlib::cv_image<dlib::rgb_pixel> dlib_cv_img, dlib::rectangle face_rect);
+    std::string predictSimilarFace(matrix<float,0,1> target_descriptor, std::vector<face_recognition_model::FaceData> reference_face_datas, float distance_threshold);
+    int registerFaceDatas(std::vector<cv::Mat> cv_imgs, std::vector<std::string> face_names);
+    int createFaceData(cv::Mat cv_img, std::string face_name, face_recognition_model::FaceData& face_data);
+    int registerFaceDatasFromCsv(std::string csv_path, std::string root_folder_path);
+
+    // util
+    std::vector<std::string> split_text(const std::string s, char delim);
+
+    // debug
+    const bool kDebugMode = true;
+};
+
+}  // namespace face_recognition_model
+}}}  // namespace nvidia::inferenceserver::custom
