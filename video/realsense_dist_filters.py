c2467f78d991dd90bd0d1d2c32fc03d5a594515e
aircampro <147149143+aircampro@users.noreply.github.com>
2026-04-06 15:11:05 +0100

    Update realsense_dist_filters.py

--- video/realsense_dist_filters.py
+++ video/realsense_dist_filters.py
@@ -6,6 +6,38 @@ import numpy as np
 import cv2
 import time
 
+class Kalman():
+    #
+    # Reference: http://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
+    #
+
+    def __init__(self, procvar, measvar, db=False):
+        self.ctr = 0
+        self.Q = procvar  # process variance
+        self.P_k_minus_1 = 0.0  # a posteri error estimate
+        self.R = measvar  # estimate of measurement variance, change to see effect
+        self.DEBUG = db
+       
+    def guess(self, input):
+
+        if self.ctr == 0:
+            self.xhat_k_minus_1 = input  # a posteri estimate of x
+            self.K = 1.0  # Kalman gain
+            self.ctr = self.ctr + 1
+            return input
+        else:
+            # time update
+            xhat_k = self.xhat_k_minus_1 # a prior estimate of x, transition matrix is identity, no control
+            Phat_k = self.P_k_minus_1 + self.Q # a prior estimate of error
+            # measurement update
+            self.K = Phat_k / (Phat_k + self.R) # Kalman gain
+            estimate = xhat_k + self.K * (input - xhat_k) # a posteri estimate of x
+            self.xhat_k_minus_1 = estimate # a posteri estimate of x
+            self.P_k_minus_1 = (1 - self.K) * Phat_k # a posteri error estimate
+            # error variance and kalman gain should become stable soon
+            if self.DEBUG: print("Kalman:","Input",input,"Estimate",int(estimate), "ErrorVar {:.2}".format(self.P_k_minus_1), "KalmanGain {:.2}".format(self.K))
+            return estimate
+            
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
@@ -40,11 +72,13 @@ try:
             period = 0.01
             y0 = (1-a) * distance 
             time.sleep(period)
+            k = Kalman(0.1, distance)                     # create kalman filter           
             first = false
         else:
             out = a*y0 + ((1-a) * distance )
             y0 = out
-            print(f" distance = {distance} output from LPF Filter : {out}")
+            kalm = k.guess(distance)
+            print(f" distance = {distance} output from LPF Filter : {out} Kalman {kalm}")
             time.sleep(period)	
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
