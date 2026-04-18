06d096d0b871dfb884820ee59736390f156c3fdb
aircampro <147149143+aircampro@users.noreply.github.com>
2026-04-18 17:09:56 +0100

    Add files via upload

---
+++ video/terrain_realsense.py
@@ -0,0 +1,507 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Show Terrain using Realsense camera save as a pointcloud ply with key 'e' quit 'q'
+#
+"""
+Keyboard:
+    [p]     Pause
+    [r]     Reset View
+    [d]     Cycle through decimation values
+    [z]     Toggle point scaling
+    [c]     Toggle color source
+    [s]     Save PNG (./out.png)
+    [e]     Export points to ply (./out.ply)
+    [q\ESC] Quit
+	
+	ref:- https://github.com/basalte1199/terrainshare_new/blob/main/terrainshare_new.py
+	ref:- https://qiita.com/basalte/items/e801a61ce4539d220e75
+
+"""
+import math
+import time
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+import json
+import matplotlib.pyplot as plt
+
+class AppState:
+
+    def __init__(self, *args, **kwargs):
+        self.WIN_NAME = 'RealSense'
+        self.pitch, self.yaw = math.radians(0), math.radians(-15)
+        self.translation = np.array([0, 0, -1], dtype=np.float32)
+        self.distance = 2
+        self.prev_mouse = 0, 0
+        self.mouse_btns = [False, False, False]
+        self.paused = False
+        self.decimate = 1
+        self.scale = True
+        self.color = True
+        self.elevation = False
+
+    def reset(self):
+        self.pitch, self.yaw, self.distance = 0, 0, 2
+        self.translation[:] = 0, 0, -1
+
+    @property
+    def rotation(self):
+        Rx, _ = cv2.Rodrigues((self.pitch, 0, 0))
+        Ry, _ = cv2.Rodrigues((0, self.yaw, 0))
+        return np.dot(Ry, Rx).astype(np.float32)
+
+    @property
+    def pivot(self):
+        return self.translation + np.array((0, 0, self.distance), dtype=np.float32)
+
+state = AppState()
+
+jsonObj = json.load(open("param.json"))
+
+# Configure depth and color streams
+# RealSense start-up
+pipeline = rs.pipeline()
+config = rs.config()
+#only need serial number for multi-cam
+config.enable_device('146322074637')
+
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+# Get stream profile and camera intrinsics
+profile = pipeline.get_active_profile()
+depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+depth_intrinsics = depth_profile.get_intrinsics()
+w, h = depth_intrinsics.width, depth_intrinsics.height
+
+# Processing blocks
+pc = rs.pointcloud()
+decimate = rs.decimation_filter()
+decimate.set_option(rs.option.filter_magnitude, 2 ** state.decimate)
+colorizer = rs.colorizer()
+
+align_to =rs.stream.color
+align = rs.align(align_to)
+
+# mouse actions
+def mouse_cb(event, x, y, flags, param):
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        state.mouse_btns[0] = True
+
+    if event == cv2.EVENT_LBUTTONUP:
+        state.mouse_btns[0] = False
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        state.mouse_btns[1] = True
+
+    if event == cv2.EVENT_RBUTTONUP:
+        state.mouse_btns[1] = False
+
+    if event == cv2.EVENT_MBUTTONDOWN:
+        state.mouse_btns[2] = True
+
+    if event == cv2.EVENT_MBUTTONUP:
+        state.mouse_btns[2] = False
+
+    if event == cv2.EVENT_MOUSEMOVE:
+
+        h, w = out.shape[:2]
+        dx, dy = x - state.prev_mouse[0], y - state.prev_mouse[1]
+
+        if state.mouse_btns[0]:
+            state.yaw += float(dx) / w * 2
+            state.pitch -= float(dy) / h * 2
+
+        elif state.mouse_btns[1]:
+            dp = np.array((dx / w, dy / h, 0), dtype=np.float32)
+            state.translation -= np.dot(state.rotation, dp)
+
+        elif state.mouse_btns[2]:
+            dz = math.sqrt(dx**2 + dy**2) * math.copysign(0.01, -dy)
+            state.translation[2] += dz
+            state.distance -= dz
+
+    if event == cv2.EVENT_MOUSEWHEEL:
+        dz = math.copysign(0.1, flags)
+        state.translation[2] += dz
+        state.distance -= dz
+
+    state.prev_mouse = (x, y)
+
+cv2.namedWindow(state.WIN_NAME, cv2.WINDOW_NORMAL)
+#cv2.resizeWindow(state.WIN_NAME, 100, 100)
+cv2.resizeWindow(state.WIN_NAME, w, h)
+cv2.setMouseCallback(state.WIN_NAME, mouse_cb)
+
+cv2.namedWindow("overview", cv2.WINDOW_NORMAL)
+#cv2.setWindowProperty('overview', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+def project(v):
+    """project 3d vector array to 2d"""
+    h, w = out.shape[:2]
+    view_aspect = float(h)/w
+
+    # ignore divide by zero for invalid depth
+    with np.errstate(divide='ignore', invalid='ignore'):
+        proj = v[:, :-1] / v[:, -1, np.newaxis] * \
+            (w*view_aspect, h) + (w/2.0, h/2.0)
+
+    # near clipping
+    znear = 0.03
+    proj[v[:, 2] < znear] = np.nan
+    return proj
+
+def view(v):
+    """apply view transformation on vector array"""
+    return np.dot(v - state.pivot, state.rotation) + state.pivot - state.translation
+
+def line3d(out, pt1, pt2, color=(0x80, 0x80, 0x80), thickness=1):
+    """draw a 3d line from pt1 to pt2"""
+    p0 = project(pt1.reshape(-1, 3))[0]
+    p1 = project(pt2.reshape(-1, 3))[0]
+    if np.isnan(p0).any() or np.isnan(p1).any():
+        return
+    p0 = tuple(p0.astype(int))
+    p1 = tuple(p1.astype(int))
+    rect = (0, 0, out.shape[1], out.shape[0])
+    inside, p0, p1 = cv2.clipLine(rect, p0, p1)
+    if inside:
+        cv2.line(out, p0, p1, color, thickness, cv2.LINE_AA)
+
+# Rendering calculation
+def grid(out, pos, rotation=np.eye(3), size=1, n=10, color=(0x80, 0x80, 0x80)):
+    """draw a grid on xz plane"""
+    pos = np.array(pos)
+    s = size / float(n)
+    s2 = 0.5 * size
+    for i in range(0, n+1):
+        x = -s2 + i*s
+        line3d(out, view(pos + np.dot((x, 0, -s2), rotation)),
+               view(pos + np.dot((x, 0, s2), rotation)), color)
+    for i in range(0, n+1):
+        z = -s2 + i*s
+        line3d(out, view(pos + np.dot((-s2, 0, z), rotation)), view(pos + np.dot((s2, 0, z), rotation)), color)
+
+def axes(out, pos, rotation=np.eye(3), size=0.075, thickness=2):
+    """draw 3d axes"""
+    line3d(out, pos, pos +
+           np.dot((0, 0, size), rotation), (0xff, 0, 0), thickness)
+    line3d(out, pos, pos +
+           np.dot((0, size, 0), rotation), (0, 0xff, 0), thickness)
+    line3d(out, pos, pos +
+           np.dot((size, 0, 0), rotation), (0, 0, 0xff), thickness)
+
+def frustum(out, intrinsics, color=(0x40, 0x40, 0x40)):
+    """draw camera's frustum"""
+    orig = view([0, 0, 0])
+    w, h = intrinsics.width, intrinsics.height
+
+    for d in range(1, 6, 2):
+        def get_point(x, y):
+            p = rs.rs2_deproject_pixel_to_point(intrinsics, [x, y], d)
+            line3d(out, orig, view(p), color)
+            return p
+
+        top_left = get_point(0, 0)
+        top_right = get_point(w, 0)
+        bottom_right = get_point(w, h)
+        bottom_left = get_point(0, h)
+
+        line3d(out, view(top_left), view(top_right), color)
+        line3d(out, view(top_right), view(bottom_right), color)
+        line3d(out, view(bottom_right), view(bottom_left), color)
+        line3d(out, view(bottom_left), view(top_left), color)
+
+#Drawing on both the projector and the PC screen
+def pointcloud(out, verts, texcoords, color, painter=True):
+    """draw point cloud with optional painter's algorithm"""
+    if painter:
+        # Painter's algo, sort points from back to front
+
+        # get reverse sorted indices by z (in view-space)
+        # https://gist.github.com/stevenvo/e3dad127598842459b68
+        v = view(verts)
+        s = v[:, 2].argsort()[::-1]
+        proj = project(v[s])
+    else:
+        proj = project(view(verts))
+
+    if state.scale:
+        proj *= 0.5**state.decimate
+
+    h, w = out.shape[:2]
+
+    # proj now contains 2d image coordinates
+    j, i = proj.astype(np.uint32).T
+
+    # create a mask to ignore out-of-bound indices
+    im = (i >= 0) & (i < h)
+    jm = (j >= 0) & (j < w)
+    m = im & jm
+
+    cw, ch = color.shape[:2][::-1]
+    if painter:
+        # sort texcoord with same indices as above
+        # texcoords are [0..1] and relative to top-left pixel corner,
+        # multiply by size and add 0.5 to center
+        v, u = (texcoords[s] * (cw, ch) + 0.5).astype(np.uint32).T
+    else:
+        v, u = (texcoords * (cw, ch) + 0.5).astype(np.uint32).T
+    # clip texcoords to image
+    np.clip(u, 0, ch-1, out=u)
+    np.clip(v, 0, cw-1, out=v)
+
+    # perform uv-mapping
+    out[i[m], j[m]] = color[u[m], v[m]]
+
+out = np.empty((h, w, 3), dtype=np.uint8)
+
+#Parameter settings for artag
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--width", help='cap width', type=int, default=960)
+    parser.add_argument("--height", help='cap height', type=int, default=540)
+
+    parser.add_argument("--families", type=str, default='tag36h11')
+    parser.add_argument("--nthreads", type=int, default=1)
+    parser.add_argument("--quad_decimate", type=float, default=1.5)
+    parser.add_argument("--quad_sigma", type=float, default=0.0)
+    parser.add_argument("--refine_edges", type=int, default=1)
+    parser.add_argument("--decode_sharpening", type=float, default=0.25)
+    parser.add_argument("--debug", type=int, default=0)
+
+    args = parser.parse_args()
+
+    return args
+
+# 
+def draw_tags(
+    image,
+    tags,
+    elapsed_time,
+    aligend_depth_frame,
+    lastest_position,
+    lastest_center
+):
+    for tag in tags:
+        tag_family = tag.tag_family
+        tag_id = tag.tag_id
+        center = tag.center
+        corners = tag.corners
+
+        depth_data = aligend_depth_frame.get_distance(int(center[0]),int(center[1]))
+
+        center = (int(center[0]), int(center[1]))
+        corner_01 = (int(corners[0][0]), int(corners[0][1]))
+        corner_02 = (int(corners[1][0]), int(corners[1][1]))
+        corner_03 = (int(corners[2][0]), int(corners[2][1]))
+        corner_04 = (int(corners[3][0]), int(corners[3][1]))
+
+        # Normally, this area of rendering is actually unnecessary (for debugging purposes)
+        cv2.circle(image, (center[0], center[1]), 5, (0, 0, 255), 2)
+
+        # Each side
+        cv2.line(image, (corner_01[0], corner_01[1]), (corner_02[0], corner_02[1]), (255, 0, 0), 2)
+        cv2.line(image, (corner_02[0], corner_02[1]), (corner_03[0], corner_03[1]), (255, 0, 0), 2)
+        cv2.line(image, (corner_03[0], corner_03[1]), (corner_04[0], corner_04[1]), (0, 255, 0), 2)
+        cv2.line(image, (corner_04[0], corner_04[1]), (corner_01[0], corner_01[1]), (0, 255, 0), 2)
+
+        cv2.line(image, (0,center[1]), (1280,center[1]), (0, 0, 0), 2)
+        cv2.line(image, (center[0],0), (center[0],720), (0, 0, 0), 2)
+
+        # Tag family, Tag ID
+        # cv2.putText(image,
+        #            str(tag_family) + ':' + str(tag_id),
+        #            (corner_01[0], corner_01[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+        #            0.6, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, "id: " + str(tag_id), (center[0] - 10, center[1] - 10),
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, "Depth " + str(int(depth_data * 100)) + "cm", (center[0] - 40, center[1] - 40),
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+    cv2.putText(image,
+               "Elapsed Time:" + '{:.1f}'.format(elapsed_time * 1000) + "ms",
+               (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2,
+               cv2.LINE_AA)
+
+    #print(lastest_center)
+    return image, lastest_center
+
+elapsed_time = 0
+lastest_position = []
+lastest_center = []
+
+while True:
+    # Grab camera data
+    if not state.paused:
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        aligned_frames = align.process(frames)
+
+        aligend_depth_frame = aligned_frames.get_depth_frame()
+        aligend_color_frame = aligned_frames.get_color_frame()
+        aligend_depth_frame_distance = aligend_depth_frame.as_depth_frame()
+
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+
+        depth_frame = decimate.process(depth_frame)
+
+        # Grab new intrinsics (may be changed by decimation)
+        depth_intrinsics = rs.video_stream_profile(depth_frame.profile).get_intrinsics()
+        w, h = depth_intrinsics.width, depth_intrinsics.height
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        aligend_depth_image = np.asanyarray(aligend_depth_frame.get_data())
+        aligend_color_image = np.asanyarray(aligend_color_frame.get_data())
+
+        depth_colormap = np.asanyarray(
+            colorizer.colorize(depth_frame).get_data())
+
+        if state.color:
+            mapped_frame, color_source = color_frame, color_image
+        else:
+            mapped_frame, color_source = depth_frame, depth_colormap
+
+        points = pc.calculate(depth_frame)
+        pc.map_to(mapped_frame)
+
+        # Pointcloud data to arrays
+        v, t = points.get_vertices(), points.get_texture_coordinates()
+        verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+        texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+
+    # Render
+    now = time.time()
+    out.fill(0)
+
+    grid(out, (0, 0.5, 1), size=1, n=10)
+    frustum(out, depth_intrinsics)
+    axes(out, view([0, 0, 0]), state.rotation, size=0.1, thickness=1)
+
+    if not state.scale or out.shape[:2] == (h, w):
+        pointcloud(out, verts, texcoords, color_source)
+    else:
+        tmp = np.zeros((h, w, 3), dtype=np.uint8)
+        pointcloud(tmp, verts, texcoords, color_source)
+        tmp = cv2.resize(
+            tmp, out.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+        np.putmask(out, tmp > 0, tmp)
+
+    if any(state.mouse_btns):
+        axes(out, view(state.pivot), state.rotation, thickness=4)
+
+    dt = time.time() - now
+
+    cv2.setWindowTitle(
+        state.WIN_NAME, "RealSense (%dx%d) %dFPS (%.2fms) %s" %
+        ( w, h, 1.0/dt, dt*1000, "PAUSED" if state.paused else ""))
+    
+    depth_min = 500
+    depth_max = 1000
+    depth_range = depth_max - depth_min
+
+    # Convert distance information into a color scale image
+    depth_mask = np.logical_and(aligend_depth_image >= depth_min, aligend_depth_image <= depth_max)
+    depth_normalized = (aligend_depth_image - depth_min) / depth_range
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_normalized, alpha=255.0), cv2.COLORMAP_JET)
+    depth_colormap[~depth_mask] = [0, 0, 0]
+
+    # Draw contour lines from three-dimensional coordinate data x, y, z
+    aligend_depth_data = np.asanyarray(aligend_depth_frame.get_data())
+    aligend_depth_frame_distance = aligend_depth_data.astype(np.uint8)
+    #contours, _ = cv2.findContours(aligend_depth_frame_distance, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.drawContours(depth_colormap, contours, -2, (255,255,255), 2)
+
+    # Invert the color map
+    depth_colormap = cv2.cvtColor(depth_colormap, cv2.COLOR_BGR2RGB)
+    ksize=201
+    img_mask = cv2.medianBlur(depth_colormap,ksize)
+    cv2.imshow(state.WIN_NAME, out)
+
+    # get the command line args
+    args = get_args()
+    cap_device = args.device
+    cap_width = args.width
+    cap_height = args.height
+
+    families = args.families
+    nthreads = args.nthreads
+    quad_decimate = args.quad_decimate
+    quad_sigma = args.quad_sigma
+    refine_edges = args.refine_edges
+    decode_sharpening = args.decode_sharpening
+    debug = args.debug
+
+    at_detector = Detector(
+        families=families,
+        nthreads=nthreads,
+        quad_decimate=quad_decimate,
+        quad_sigma=quad_sigma,
+        refine_edges=refine_edges,
+        decode_sharpening=decode_sharpening,
+        debug=debug,
+    )
+
+    tags = at_detector.detect(
+            color_image,
+            estimate_tag_pose=False,
+            camera_params=None,
+            tag_size=None,
+    )
+    debug_image, lastest_position = draw_tags(depth_colormap, tags, elapsed_time, aligend_depth_frame, lastest_position, lastest_center)
+    print(lastest_position)
+
+    cv2.imshow("overview", debug_image)
+    key = cv2.waitKey(1)
+    # control keys for actions
+    if key == ord("r"):
+        state.reset()
+
+    if key == ord("p"):
+        state.paused ^= True
+
+    if key == ord("d"):
+        state.decimate = (state.decimate + 1) % 3
+        decimate.set_option(rs.option.filter_magnitude, 2 ** state.decimate)
+
+    if key == ord("z"):
+        state.scale ^= True
+
+    if key == ord("c"):
+        state.color ^= True
+
+    if key == ord("s"):
+        cv2.imwrite('./out.png', out)
+
+    if key == ord("e"):
+        points.export_to_ply('./out.ply', mapped_frame)
+
+    if key in (27, ord("q")) or cv2.getWindowProperty(state.WIN_NAME, cv2.WND_PROP_AUTOSIZE) < 0 or cv2.getWindowProperty("overview", cv2.WND_PROP_AUTOSIZE) < 0:
+        break
+
+# Stop streaming
+pipeline.stop()
